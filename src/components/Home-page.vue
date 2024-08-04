a6009a5e0799303a51b63d7e26d39257501d64b6
bhupesh <bhupeshxrma24233@gmail.com>
2024-08-04 11:33:06 +0530

    new lessions till two-way-Binding

---
+++ src/components/Home-page.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1>Home Page</h1>
+  <h1>{{email}}</h1>
+<h1> name: : {{getName("Bhupesh")}}</h1>
+<h1> Email : {{getEmail().age}}  </h1>
+<button v-on:click="getInc()">click me</button>
+<button v-on:dblclick="getData('this is double click happening')">click here</button>
+<h1>{{count}}</h1>
+<input type="text" v-model = "Inc"/>
+<h1>{{Inc}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data(){
+    return{
+      Inc : 10,
+      count : 0,
+      email:"Bhupesh@23",
+    }
+  },
+  methods:{
+
+    getInc(){
+       this.count = this.count+1
+    },
+   
+    getName(name){
+      return name
+    },
+
+    getEmail(){
+      return {
+        
+       email: this.email,
+       age : '21'
+    }
+    },
+    getData(data){
+    alert(data)
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+h1{
+  color : green;
+}
+
+</style>
---
+++ src/components/SecHome.vue
@@ -0,0 +1,34 @@
+<template>
+<h1>This is my Second Home</h1>
+<br />
+<br/>
+<input type="text" placeholder="Your Email" v-model="Email" />
+<br />
+<br />
+<input type="password" placeholder="Your Password"  v-model="Password"/>
+<br/>
+<br/>
+<button type="button" v-on:click="getData()">Submit</button>
+</template>
+
+<script>
+ export default{
+    name: 'SecHome',
+    data(){
+        return {
+        Email : null,
+        Password: null,
+        }
+    },
+    methods:{
+        getData(){
+           console.warn(this.Email,this.Password);
+           
+        }
+
+    }
+}
+</script>
+
+<style>
+</style>
