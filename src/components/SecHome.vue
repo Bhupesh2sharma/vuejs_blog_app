0fdbff9dc6a6264488549a788ac26d2543cba823
bhupesh <bhupeshxrma24233@gmail.com>
2024-08-04 16:45:43 +0530

    if else in vue

--- src/components/SecHome.vue
+++ src/components/SecHome.vue
@@ -8,7 +8,43 @@
 <input type="password" placeholder="Your Password"  v-model="Password"/>
 <br/>
 <br/>
+<h4>Technology</h4><br />
+<label for="Node">Node</label>
+<input type="checkbox" value="Node" v-model="technology" id="Node"/>
+<br>
+<label for="React">React</label>
+<input type="checkbox" value="React" v-model="technology" id="React"/>
+<br />
+<label for="Js">Js</label>
+<input type="checkbox" value="Js" v-model="technology" id="Js"/>
+
+<br/>
+<br/>
+<h3>Who Am I</h3>
+<br />
+<label for="student">Student</label>
+<input type="radio" value="student" v-model="who" id="student"/>
+<br/>
+<label for="developer">Developer</label>
+<input type="radio" value="developer" v-model="who" id="developer"/>
+
+<h3>Selected Technology: {{technology}}</h3>
+<br/>
+<br/>
+<h3>Selected Who: {{who}}</h3>
 <button type="button" v-on:click="getData()">Submit</button>
+
+<br/>
+<br/>
+
+<h3 v-if="show">If true show this </h3>
+
+<h3 v-else>If false Show this </h3>
+
+<br/>
+<button v-on:click="show=!show">Toggle</button>
+
+
 </template>
 
 <script>
@@ -18,6 +54,9 @@
         return {
         Email : null,
         Password: null,
+        technology:[],
+        who:null,
+        show: false
         }
     },
     methods:{
